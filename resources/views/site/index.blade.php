e4954fa4d42fc0ce7db23e468aafedb0a2e8af21
sthefany-bn <sthy1917@gmail.com>
2023-08-15 15:05:34 -0300

    2º

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -1,5 +1,29 @@
 @extends('site.layout')
 @section('title', 'Descarte aqui')
 @section('body')
-    
+    <div class="v1_2">
+        <!--img-->
+        <div class="v5_40"></div>
+        <div class="v6_64"></div>
+        <div class="v6_47"></div>
+        <div class="v6_49"></div>
+        <div class="v6_50"></div>
+        <div class="v6_54"></div>
+        <span class="v7_71">ORGÂNICO</span>
+        <span class="v7_67">PAPEL</span>
+        <span class="v7_66">PLÁSTICO</span>
+        <span class="v7_68">VIDRO</span>
+        <div class="v6_55"></div>
+        <div class="v6_46"></div>
+        <div class="v6_51"></div>
+        <div class="v6_52"></div>
+        <div class="v6_53"></div>
+        <div class="v6_58"></div>
+        <div class="v6_59"></div>
+        <span class="v7_65">PAPEL</span>
+        <span class="v7_69">METAL</span>
+        <span class="v7_70">ÓLEO</span>
+        <div class="v7_74"></div>
+        <span class="v7_73">Proximos Agendamentos</span>
+    </div>
 @endsection
